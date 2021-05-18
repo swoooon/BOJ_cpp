0f342c58d1aaa25092be9194c7010cf14c137150
blueint323 <blueinthe@naver.com>
2021-05-19 01:21:51 +0900

    template

---
+++ template/Matrix.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+
+const int mod = 1e6+3;
+
+struct Matrix{
+	int size;
+	vector< vector<ll> > arr;
+	Matrix(){size = 0;}
+	Matrix(int n){
+		size = n;
+		arr = vector< vector<ll> >(n, vector<ll>(n));
+	}
+	Matrix operator * (const Matrix &x){
+		Matrix ret(size);
+		for(int i=0; i<size; i++){
+			for(int j=0; j<size; j++){
+				for(int k=0; k<size; k++){
+					ret.arr[i][j] += arr[i][k] * x.arr[k][j];
+					ret.arr[i][j] %= mod;
+				}
+			}
+		}
+		return ret;
+	}
+};
+
+Matrix powmat(Matrix a, ll b){
+	if(b == 1) return a;
+	Matrix ret = powmat(a, b/2);
+	ret = ret * ret;
+	if(b & 1) ret = ret * a;
+	return ret;
+}
---
+++ template/default.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pii;
+
+const ll mod = 1e9+7;
+
+
+ll gcd(ll a, ll b) {
+    if (b) return gcd(b, a%b);
+    return a;
+}
+
+ll ceil(ll a, ll b) {
+    return (a-1)/b + 1;
+}
+
+void debug() {
+
+}
+
+
+
+
+
+void solve() {
+    
+}
+
+
+
+int main() {
+    FASTIO
+    ll T=1;
+    while (T--) solve();
+} 
