b2c3fd46657d1a0b25fd1fe9cb188058ddb498dd
blueint323 <blueinthe@naver.com>
2021-08-02 09:51:42 +0900

    Update Segtree.cpp

---
+++ template/Segtree.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pii;
+typedef long double ld;
+#define ct(x) cout << x << '\n'
+#define ct2(x, y) cout << x << ' ' << y << '\n'
+#define ct3(x, y, z) cout << x << ' ' << y << ' ' << z << '\n'
+#define ct4(x, y, z, w) cout << x << ' ' << y << ' ' << z << ' ' << w << '\n'
+const ll NMAX = 2010101;
+const ll mod = 1e9+7;
+int dy[4] = {1, 0, -1, 0}, dx[4] = {0, 1, 0, -1};
+
+
+ll gcd(ll a, ll b) {
+    if (b) return gcd(b, a%b);
+    return a;
+}
+
+ll ceil(ll a, ll b) {
+    return (a-1)/b + 1;
+}
+
+
+class SegmentTree {
+public:
+	vector<ll> seg;
+	int n;
+	SegmentTree(int n) {
+		seg.resize(4 * n + 5);
+		this->n = n;
+	}
+	ll upd_(int idx, int l, int r, int pos, ll val) {
+		if (pos < l || pos > r) return seg[idx];
+		if (pos == l && pos == r) return seg[idx] = val;
+		int mid = (l + r) / 2;
+		return seg[idx] = gcd(
+			upd_(idx * 2, l, mid, pos, val),
+			upd_(idx * 2 + 1, mid + 1, r, pos, val)
+			);
+	}
+	ll calc_(int idx, int l, int r, int tl, int tr) {
+		if (tl > tr) return 0;
+		if (tl == l && tr == r) return seg[idx];
+		int mid = (l + r) / 2;
+		return gcd(
+			calc_(idx * 2, l, mid, tl, min(tr, mid)),
+			calc_(idx * 2 + 1, mid + 1, r, max(mid + 1, tl), tr)
+			);
+	}
+	void upd(int pos, ll val) {
+		upd_(1, 0, n - 1, pos, val);
+	}
+	ll calc(int l, int r) {
+		return calc_(1, 0, n - 1, l, r);
+	}
+};
