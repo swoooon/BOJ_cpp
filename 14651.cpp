0b0a9fe7aa30502a9e49149b228c008d46ff73fa
blueint323 <blueinthe@naver.com>
2021-08-07 13:29:24 +0900

    Create 14651.cpp

---
+++ 14651.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pii;
+typedef long double ld;
+#define ct(x) cout << x << '\n'
+#define ct2(x, y) cout << x << ' ' << y << '\n'
+#define ct3(x, y, z) cout << x << ' ' << y << ' ' << z << '\n'
+#define ct4(x, y, z, w) cout << x << ' ' << y << ' ' << z << ' ' << w << '\n'
+
+const ll mod = 1e9+7;
+int dy[4] = {1, 0, -1, 0}, dx[4] = {0, 1, 0, -1};
+
+
+ll gcd(ll a, ll b) {
+    if (b) return gcd(b, a%b);
+    return a;
+}
+
+ll ceil(ll a, ll b) {
+    return (a-1)/b + 1;
+}
+
+void debug() {
+
+}
+
+ll dp[33334][3];
+
+ll f(ll n, ll m) {
+    if (n == 1) {
+        if (m == 0) return 0;
+        return 1;
+    }
+    ll &ret = dp[n][m];
+    if (ret) return ret;
+    return ret = (f(n-1, 0) + f(n-1, 1) + f(n-1, 2)) % (mod+2);
+}
+
+void solve() {
+    int N; cin >> N;
+    ct(f(N, 0));
+}
+
+
+
+int main() {
+    FASTIO
+    ll T=1;
+    while (T--) solve();
+} 
